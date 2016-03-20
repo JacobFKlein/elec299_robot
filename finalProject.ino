bc01e804f870451ba5846358c34f87278e60ed3f
Jacob Klein <jacobfk@live.com>
2016-03-19 20:39:54 -0400

    Update finalProject.ino
    - Cleaned up code  (still more work needs to be done)
    - Included IRSensor attached below robot to determine whether on black tape or not.
    - Added FollowLine function (still needs to be tested)

--- finalProject.ino
+++ finalProject.ino
@@ -1,48 +1,51 @@
-byte left_speed = 126;
-byte right_speed = 114; //PWM values from 0 to 255
-
 #include <Servo.h>
 #define TILT 9
 #define PAN 8
 #define GRIP 10
 #define PRESSURE A1 //NEED TO MOVE TO ANOTHER ANALOG PIN
 
-#define BUTTON 12
-
-#define LEFT_WHEEL_SPEED 5
 #define LEFT_WHEEL_DIRECTION 4
+#define LEFT_WHEEL_SPEED 5
 #define RIGHT_WHEEL_SPEED 6
 #define RIGHT_WHEEL_DIRECTION 7
+
+#define IRL A5
+#define IRC A4
+#define IRR A3
+#define LTHRESH 280
+#define CTHRESH 280
+#define RTHRESH 280
 
 #define LEFT_BUMPER 11
 #define RIGHT_BUMPER 13
 
-#define DEGREE_TO_MSEC_RATIO 11
-
+byte left_speed = 126;
+byte right_speed = 114; //PWM values from 0 to 255
+Servo tilt, pan, grab;
 char BTin;
 
 void setup() {
   Initialize();
   WaitBumper();
   Serial.begin(115200);
-  digitalWrite(LEFT_WHEEL_DIRECTION, HIGH);
-  digitalWrite(RIGHT_WHEEL_DIRECTION, HIGH);
 }
 
 void loop() {
   //BTin = GetBT();
-  digitalWrite(LEFT_WHEEL_DIRECTION, HIGH);
-  digitalWrite(RIGHT_WHEEL_DIRECTION, HIGH);
-  analogWrite(LEFT_WHEEL_SPEED, 0); /* 1-255 over 200 fairly unstable */
-  analogWrite(RIGHT_WHEEL_SPEED, 0);
-  delay(5000);
+  delay(2500);
   SPIN(90, 'L');
   switch (BTin) {
     case '1': // go to left one spin, left 90
+      SPIN(90, 'L');
+      FollowLine();
       break;
     case '2': // go to right one spin, right 90
+      SPIN(90,'R');
+      FollowLine();
       break;
     case '3': // go to back one, spin 180
+      SPIN(180, 'L');
+      FollowLine();
       break;
     default: //otherwise do...
       break;
@@ -51,16 +54,17 @@ void loop() {
 
 void Initialize() {
   /* Declaring inputs and outputs */
-  pinMode(BUTTON, INPUT); 
   pinMode(LEFT_WHEEL_SPEED, INPUT);
   pinMode(LEFT_WHEEL_DIRECTION, INPUT);
   pinMode(RIGHT_WHEEL_SPEED, INPUT);
   pinMode(RIGHT_WHEEL_DIRECTION, INPUT);
   pinMode(LEFT_BUMPER, INPUT);
   pinMode(RIGHT_BUMPER, INPUT);
+  pinMode(IRL, OUTPUT);
+  pinMode(IRC, OUTPUT);
+  pinMode(IRR, OUTPUT);
   
   /* Initializing Servos (so they dont shake) */
-  Servo tilt, pan, grab;
   tilt.attach(TILT);
   tilt.write(48);
   pan.attach(PAN);
@@ -78,11 +82,11 @@ void WaitBumper() {
     digitalRead(RIGHT_BUMPER) == LOW) {}
   while (digitalRead(LEFT_BUMPER) == HIGH &&
     digitalRead(RIGHT_BUMPER) == HIGH) {}
-  delay(100);
+  delay(250);
 }
 
 char GetBT() {
-    BTin = ' ';
+  BTin = ' ';
   if (Serial.available()) {
     BTin = Serial.read();
   }
@@ -93,20 +97,34 @@ void SPIN(int degree, char direction) {
   if (direction == 'L') {
     digitalWrite(LEFT_WHEEL_DIRECTION, LOW);
     digitalWrite(RIGHT_WHEEL_DIRECTION, HIGH);
-    analogWrite(LEFT_WHEEL_SPEED, left_speed); /* 1-255 over 200 fairly unstable */
-    analogWrite(RIGHT_WHEEL_SPEED, right_speed);
-    delay(degree*9.78);
-    analogWrite(LEFT_WHEEL_SPEED,0); /* 1-255 over 200 fairly unstable */
-    analogWrite(RIGHT_WHEEL_SPEED,0);
   }
   else if (direction == 'R') {
     digitalWrite(LEFT_WHEEL_DIRECTION, HIGH);
     digitalWrite(RIGHT_WHEEL_DIRECTION, LOW);
-    analogWrite(LEFT_WHEEL_SPEED, left_speed); /* 1-255 over 200 fairly unstable */
-    analogWrite(RIGHT_WHEEL_SPEED, right_speed);
-    delay(degree*9.78);
-    analogWrite(LEFT_WHEEL_SPEED,0); /* 1-255 over 200 fairly unstable */
-    analogWrite(RIGHT_WHEEL_SPEED,0);
+  }
+  analogWrite(LEFT_WHEEL_SPEED, left_speed); /* 1-255 over 200 fairly unstable */
+  analogWrite(RIGHT_WHEEL_SPEED, right_speed);
+  delay(degree*9.78);
+  analogWrite(LEFT_WHEEL_SPEED,0); /* 1-255 over 200 fairly unstable */
+  analogWrite(RIGHT_WHEEL_SPEED,0);
+}
+
+void FollowLine() { /* Follows line until hits wall */
+  while (digitalRead(LEFT_BUMPER) == LOW || digitalRead(RIGHT_BUMPER) == LOW) {
+    digitalWrite(LEFT_WHEEL_DIRECTION, HIGH);
+    digitalWrite(RIGHT_WHEEL_DIRECTION, HIGH);
+    while (CenterSensor < CTHRESH) {
+      analogWrite(LEFT_WHEEL_SPEED, left_speed); /* 1-255 over 200 fairly unstable */
+      analogWrite(RIGHT_WHEEL_SPEED, right_speed);
+    }
+    if (LeftSensor < LTHRESH) {
+      analogWrite(LEFT_WHEEL_SPEED, left_speed-10); /* 1-255 over 200 fairly unstable */
+      analogWrite(RIGHT_WHEEL_SPEED, right_speed+10);
+    }
+      if (RightSensor < RTHRESH) {
+        analogWrite(LEFT_WHEEL_SPEED, left_speed+10); /* 1-255 over 200 fairly unstable */
+        analogWrite(RIGHT_WHEEL_SPEED, right_speed-10);
+    }
   }
 }
 
