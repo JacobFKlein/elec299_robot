22fd091c446b329e1b65c2f3d1a8794a850f8a9c
Jacob Klein <jacobfk@live.com>
2016-03-22 14:23:02 -0400

    update finalProject.ino
    Commented code
    Fixed FollowLine function (still needs testing)

--- finalProject/finalProject.ino
+++ finalProject/finalProject.ino
@@ -1,14 +1,19 @@
 #include <Servo.h>
+
+//servo pins and variables
 #define TILT 9
 #define PAN 8
 #define GRIP 10
 #define PRESSURE A1 //NEED TO MOVE TO ANOTHER ANALOG PIN
+Servo tilt, pan, grab;
 
+//wheel pins
 #define LEFT_WHEEL_DIRECTION 4
 #define LEFT_WHEEL_SPEED 5
 #define RIGHT_WHEEL_SPEED 6
 #define RIGHT_WHEEL_DIRECTION 7
 
+//light sensor pins and thresholds
 #define LeftLightSensor A5
 #define CenterLightSensor A4
 #define RightLightSensor A3
@@ -16,13 +21,23 @@
 #define CTHRESH 750
 #define RTHRESH 750
 
+//bumper pins
 #define LEFT_BUMPER 11
 #define RIGHT_BUMPER 13
 
+//wheel speeds that allow robot to drive straight
 byte left_speed = 126;
-byte right_speed = 114; //PWM values from 0 to 255
-Servo tilt, pan, grab;
+byte right_speed = 114; 
+
+//global bluetooth input variable
 char BTin;
+
+/* current position variable
+  values(Left,Center,Right,Front,Back) */
+String position = "Center";
+/* next position variable
+  values(Left,Center, Right,Front,Back) */
+String nextPosition;
 
 void setup() {
   Initialize();
@@ -55,6 +70,7 @@ void loop() {
   }
 }
 
+//used to initialize all pins and servos in the robot
 void Initialize() {
   /* Declaring inputs and outputs */
   pinMode(LEFT_WHEEL_SPEED, OUTPUT);
@@ -63,14 +79,12 @@ void Initialize() {
   pinMode(RIGHT_WHEEL_DIRECTION, OUTPUT);
   pinMode(LEFT_BUMPER, INPUT);
   pinMode(RIGHT_BUMPER, INPUT);
-  
   pinMode(LeftLightSensor, INPUT);
   pinMode(CenterLightSensor, INPUT);
   pinMode(RightLightSensor, INPUT);
   
   /* Initializing Servos (so they dont shake) */
   tilt.attach(TILT);
-  // tilt.write(48); FOR GRABBALL
   tilt.write(120);
   pan.attach(PAN);
   pan.write(90);
@@ -78,12 +92,15 @@ void Initialize() {
   grab.write(80);
  }
 
+/* this function waits until a bumper is pressed
+   (similar to having a pushbutton) */
 void WaitBumper() {
   while (digitalRead(LEFT_BUMPER) == LOW &&
     digitalRead(RIGHT_BUMPER) == LOW) {}
   delay(250);
 }
 
+//gets a character response from the BT module
 char GetBT() {
   BTin = ' ';
   if (Serial.available()) {
@@ -92,6 +109,7 @@ char GetBT() {
   return BTin;
 }
 
+//spins the robot a given degree in a given direction
 void SPIN(int degree, char direction) {
   if (direction == 'L') {
     digitalWrite(LEFT_WHEEL_DIRECTION, LOW);
@@ -108,28 +126,31 @@ void SPIN(int degree, char direction) {
   analogWrite(RIGHT_WHEEL_SPEED,0);
 }
 
+/* using light sensors allows the robot to follow a
+   black line until it hits a wall */
 void FollowLine() { /* Follows line until hits wall */
   while (digitalRead(LEFT_BUMPER) == LOW || digitalRead(RIGHT_BUMPER) == LOW) {
     digitalWrite(LEFT_WHEEL_DIRECTION, HIGH);
     digitalWrite(RIGHT_WHEEL_DIRECTION, HIGH);
-    if (digitalRead(CenterLightSensor) < CTHRESH) {
+    if (analogRead(CenterLightSensor) < CTHRESH) {
       analogWrite(LEFT_WHEEL_SPEED, left_speed); /* 1-255 over 200 fairly unstable */
       analogWrite(RIGHT_WHEEL_SPEED, right_speed);
       delay(50);
     }
-    else if (digitalRead(LeftLightSensor) < LTHRESH) {
-      analogWrite(LEFT_WHEEL_SPEED, left_speed-80); /* 1-255 over 200 fairly unstable */
-      analogWrite(RIGHT_WHEEL_SPEED, right_speed+80);
+    else if (analogRead(LeftLightSensor) < LTHRESH) {
+      analogWrite(LEFT_WHEEL_SPEED, left_speed-30); /* 1-255 over 200 fairly unstable */
+      analogWrite(RIGHT_WHEEL_SPEED, right_speed+30);
       delay(50);
     }
-    else if (digitalRead(RightLightSensor) < RTHRESH) {
-      analogWrite(LEFT_WHEEL_SPEED, left_speed+80); /* 1-255 over 200 fairly unstable */
-      analogWrite(RIGHT_WHEEL_SPEED, right_speed-80);
+    else if (analogRead(RightLightSensor) < RTHRESH) {
+      analogWrite(LEFT_WHEEL_SPEED, left_speed+30); /* 1-255 over 200 fairly unstable */
+      analogWrite(RIGHT_WHEEL_SPEED, right_speed-30);
       delay(50);
     }
   }
 }
 
+//makes the robot backup for a given time
 void Backup(int timeMsec) {
   digitalWrite(LEFT_WHEEL_DIRECTION, LOW);
   digitalWrite(RIGHT_WHEEL_DIRECTION, LOW);
@@ -141,6 +162,7 @@ void Backup(int timeMsec) {
   
 }
 
+//makes the robot pickup the ball
 void PickupBall() {
   Backup(165);
   tilt.write(40);
@@ -149,5 +171,25 @@ void PickupBall() {
   grab.write(110);
   delay(450);
   Backup(1000);
+}
+
+//INCOMPLETE
+/* allows the robot to recieve the location of the next ball
+   through either Infrared or Bluetooth */
+void getNextLocation() {
+	while (true) {
+	//turn 45
+	//search ir
+	//if get a 1/2/3 from ir
+		//finish 360 turn
+		//set nextlocation to 1/2/3
+		//break;
+	//search BT
+	//if get a 1/2/3 from BT
+		//finish 360 turn
+		//set nextlocation to 1/2/3
+		//break;
+	}
+
 }
 
